266a4cc0a6c3a1271ba25e7972de75e543880bc9
duhBlu <jacobthieret2@gmail.com>
2023-09-30 12:14:17 -0500

    user can now scroll through the images in the image gallery

--- Emotion Filtering Tool/views/gallery_view.py
+++ Emotion Filtering Tool/views/gallery_view.py
@@ -19,30 +19,6 @@ class GalleryView(ttk.Frame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=0)  # This ensures scrollbar remains to the right
         self.create_widgets()
-        #self.testImage()                       # Test fill the gallery with images
-
-    def testImage(self):
-        # any folder with images
-        folder_path = "C:/Users/jacob/Desktop/temp/Humans"
-        
-        # Loop through all the files in the folder
-        for file_name in os.listdir(folder_path):
-            # Join folder path with file name to get the full path
-            file_path = os.path.join(folder_path, file_name)
-            #file_path = "C:/Users/jacob/Desktop/temp/Humans/1 (107).png"
-
-            # Check if it's a file (and not a directory) and has a .jpeg or .jpg extension
-            if os.path.isfile(file_path) and file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
-                print(file_path)
-                with open(file_path, 'rb') as f:
-                    image_data = f.read()
-                
-                image = Image.open(BytesIO(image_data))
-                self.resize_image(image)
-                self.add_image_to_gallery(image)
-            self.c += 1
-            if(self.c == 200):
-                break
 
     def create_widgets(self):
         # Configure canvas and scrollbar
@@ -53,13 +29,17 @@ class GalleryView(ttk.Frame):
         self.scrollbar.grid(row=0, column=1, sticky='nse')
         
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-
+        
         # Create frame to hold images
         self.frame_images = ttk.Frame(self.canvas)
         self.frame_images.grid_columnconfigure(0, weight=1)  # This allows column 0 in frame_images to expand with the canvas
         self.canvas.create_window((0, 0), window=self.frame_images, anchor="nw")
 
         self.frame_images.bind("<Configure>", self.on_frame_configure)
+        self.frame_images.bind("<MouseWheel>", self._on_mousewheel)
+        self.frame_images.bind("<Button-4>", self._on_mousewheel_up)
+        self.frame_images.bind("<Button-5>", self._on_mousewheel_down)
+
         self.status_label = ttk.Label(self, text="")
         self.status_label.grid(row=1, column=0, columnspan=2, sticky="w")
 
@@ -69,8 +49,16 @@ class GalleryView(ttk.Frame):
 
     def on_frame_configure(self, event=None):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+    
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(-1*(event.delta//120), "units")
 
+    def _on_mousewheel_up(self, event):
+        self.canvas.yview_scroll(-1, "units")
 
+    def _on_mousewheel_down(self, event):
+        self.canvas.yview_scroll(1, "units")
+        
     def load_images_from_folder(self, folder_paths):
         for folder_path in folder_paths:
             for img_file in os.listdir(folder_path):
@@ -107,8 +95,11 @@ class GalleryView(ttk.Frame):
         try:
             photo = ImageTk.PhotoImage(self.resize_image(image))
             self.photo_images.append(photo)
-            label = ttk.Label(self.frame_images, image=photo)
-            label.grid(row=self.current_row, column=self.current_col, sticky="nw", padx=5, pady=5)  
+            img_label = ttk.Label(self.frame_images, image=photo)
+            img_label.grid(row=self.current_row, column=self.current_col, sticky="nw", padx=5, pady=5)  
+            img_label.bind("<MouseWheel>", self._on_mousewheel)
+            img_label.bind("<Button-4>", self._on_mousewheel_up)
+            img_label.bind("<Button-5>", self._on_mousewheel_down)
         except Exception as e:
             print(f"Error displaying image: {e}")
     
