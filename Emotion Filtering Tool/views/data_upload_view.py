ca8f2677bf232bf815513e91679465c5c28531ab
duhBlu <jacobthieret2@gmail.com>
2023-11-27 20:47:45 -0600

    Improved Gallery and Review Functionality: Modified to only send images shown in the gallery. The 'Load More' button now loads up to 100 additional images and activates when 100 images are displayed, enabling further image loading as needed.

--- Emotion Filtering Tool/views/data_upload_view.py
+++ Emotion Filtering Tool/views/data_upload_view.py
@@ -114,11 +114,27 @@ class DataUploadView(ttk.Frame):
         self.width_entry = ttk.Entry(self, width=5)
         self.width_entry.grid(row=3, column=1, sticky='w', padx=(160, 0), pady=(300, 0))
         self.width_entry.insert(0, "200")  # default width
-        self.width_entry.bind("<FocusOut>", self.validate_width_entry)
+        self.width_entry.bind("<KeyRelease>", self.on_width_entry_key_release)
+        self.width_entry.bind("<FocusIn>", self.on_entry_focus)
 
     '''
     UI helper functions
     '''
+    def on_entry_focus(self, event):
+        event.widget.select_range(0, tk.END)
+        event.widget.icursor(tk.END) 
+        
+    def on_width_entry_key_release(self, event):
+        current_text = self.width_entry.get()
+        try:
+            # Try to convert the text to an integer
+            int(current_text)  # If this fails, ValueError will be raised
+            self.last_valid_width = current_text  # Update the last valid value
+        except ValueError:
+            # If conversion fails, revert to the last valid value or default
+            self.width_entry.delete(0, tk.END)
+            self.width_entry.insert(0, self.last_valid_width if hasattr(self, 'last_valid_width') else "200")
+        
     def validate_width_entry(self):
         try:
             user_width = int(self.width_entry.get())
